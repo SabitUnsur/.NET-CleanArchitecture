8d1419c3ae79e9eba8ead55af83c560ed86ebab6
Egemen Unsur <sabitozbilgic2306@gmail.com>
2024-06-29 16:54:54 +0300

    layers

---
+++ CleanArchitecture.Presentation/AssemblyReference.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Presentation
+{   
+    //bunun amacı, CleanArchitecture.Presentation assembly'sini referans olarak kullanabilmek
+    public static class AssemblyReference
+    {
+        public static readonly Assembly assembly = typeof(Assembly).Assembly;
+    }
+}
